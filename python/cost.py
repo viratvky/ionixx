c0df8be6bf7916e17dd93fe28d3d6190d57d8e2d
vignesh <43260609+viratvky@users.noreply.github.com>
2020-07-24 13:32:03 +0530

    Update cost.py

--- python/cost.py
+++ python/cost.py
@@ -5,6 +5,18 @@ while(1):
         a,b=input().split('-')
         print("Enter the input like this format 30 50")
         c,k=map(int,input().split())
+        a1=''
+        b1=''
+        c1=''
+        d1=''
+        if a=='A':
+            a1=a1+b
+        if a=='B':
+            b1=b1+b
+        if a=='C':
+            c1=c1+b
+        if a=='D':
+            d1=d1+b        
         try:
             if d[a]>c*k:
                 d[a]=c*k
@@ -17,6 +29,9 @@ while(1):
             d[b]=c*k
     except:
         break
-print(d)
+l=[]
+for i in d:
+    l.append(d[i])
+print(min(l))
         
         
